a452b33419859dcd5575b55911b7510d42081fb6
Kezi <keziolio123@gmail.com>
2025-11-01 21:15:19 +0100

    compile time settings for dithering, auto cut

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 use error::PrinterBotError;
+use image::{ImageBuffer, Luma, Rgba};
 use log::*;
 use teloxide_core::net::Download;
 use teloxide_core::types::{ChatId, FileId};
@@ -14,8 +15,11 @@ use crate::driver::{PrinterCommand, PrinterCommandMode, PrinterExpandedMode, Pri
 mod driver;
 mod error;
 
+#[derive(Debug)]
 struct Settings {
     dpi_600: bool,
+    auto_cut: bool,
+    dithering: bool,
 }
 
 #[tokio::main]
@@ -39,7 +43,15 @@ async fn main() -> Result<(), PrinterBotError> {
 
     let mut offset: u32 = 0;
 
-    let settings = Settings { dpi_600: false };
+    let settings = Settings {
+        dpi_600: false,
+        auto_cut: true,
+        dithering: true,
+    };
+
+    info!("Settings: {:?}", settings);
+    bot.send_message(owner_id, format!("Settings: {:?}", settings))
+        .await?;
 
     loop {
         let updates = bot.get_updates().offset(offset as i32).await;
@@ -134,6 +146,107 @@ async fn download_file(
     Ok(file_path)
 }
 
+fn apply_dithering(
+    mut input_img: ImageBuffer<Luma<u8>, Vec<u8>>,
+) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>, PrinterBotError> {
+    // match the brightness of the previous implementation
+    let gamma_correction = 5.14;
+
+    input_img
+        .pixels_mut()
+        .for_each(|x| x.0 = [(255.0 * (x.0[0] as f32 / 255.0).powf(1.0 / gamma_correction)) as u8]);
+
+    use exoquant::*;
+
+    let palette = vec![Color::new(0, 0, 0, 255), Color::new(255, 255, 255, 255)];
+
+    let ditherer = ditherer::FloydSteinberg::vanilla();
+    let colorspace = SimpleColorSpace::default();
+    let remapper = Remapper::new(&palette, &colorspace, &ditherer);
+
+    let image = input_img
+        .pixels()
+        .map(|x| Color::new(x.0[0], x.0[0], x.0[0], 255))
+        .collect::<Vec<Color>>();
+
+    let indexed_data = remapper.remap(&image, input_img.width() as usize);
+
+    let img = image::ImageBuffer::from_fn(input_img.width(), input_img.height(), |x, y| {
+        let i = y * input_img.width() + x;
+        let i = indexed_data[i as usize];
+        image::Rgba([i * 255, i * 255, i * 255, 255])
+    });
+
+    Ok(img)
+}
+
+fn apply_threshold(
+    mut img: ImageBuffer<Luma<u8>, Vec<u8>>,
+) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>, PrinterBotError> {
+    img.pixels_mut().for_each(|x| {
+        if x.0[0] > 128 {
+            x.0[0] = 255;
+        } else {
+            x.0[0] = 0;
+        }
+    });
+
+    let img = image::ImageBuffer::from_fn(img.width(), img.height(), |x, y| {
+        let i = y * img.width() + x;
+        let i = img.get_pixel(x, y).0[0];
+        image::Rgba([i, i, i, 255])
+    });
+
+    Ok(img)
+}
+
+fn img_to_lines(img: ImageBuffer<Rgba<u8>, Vec<u8>>) -> Result<Vec<[u8; 90]>, PrinterBotError> {
+    // convert to vec of line bits
+    /*
+        let mut lines = Vec::new();
+
+        for y in 0..img.height() {
+            let mut line = [0u8; 90];
+
+            for x in 0..img.width() {
+                let i = y * img.width() + x;
+                let i = indexed_data[i as usize];
+
+                let byte = x / 8;
+                let bit = x % 8;
+
+                if i == 0 {
+                    line[89 - byte as usize] |= 1 << bit;
+                }
+            }
+
+            lines.push(line);
+        }
+    */
+
+    let mut lines = Vec::new();
+
+    for y in 0..img.height() {
+        let mut line = [0u8; 90];
+
+        for x in 0..img.width() {
+            let i = img.get_pixel(x, y).0[0];
+
+            let byte = x / 8;
+            let bit = x % 8;
+
+            // if the pixel is black, set the bit so it's printed (in black)
+            if i == 0 {
+                line[89 - byte as usize] |= 1 << bit;
+            }
+        }
+
+        lines.push(line);
+    }
+
+    Ok(lines)
+}
+
 fn render_image(file_path: &str, settings: &Settings) -> Result<Vec<[u8; 90]>, PrinterBotError> {
     use image::ImageReader;
 
@@ -143,7 +256,7 @@ fn render_image(file_path: &str, settings: &Settings) -> Result<Vec<[u8; 90]>, P
 
     let ratio = img.height() as f32 / img.width() as f32;
 
-    if ratio > 1.5 {
+    if ratio > 3.5 {
         println!("Ratio is too high: {}", ratio);
         return Err(PrinterBotError::InvalidImage);
     }
@@ -173,52 +286,15 @@ fn render_image(file_path: &str, settings: &Settings) -> Result<Vec<[u8; 90]>, P
         image::imageops::FilterType::Lanczos3,
     );
 
-    // gamma correction
-    // match the brightness of the previous implementation
-    let gamma_correction = 5.14;
+    let dithered_img = if settings.dithering {
+        apply_dithering(img)?
+    } else {
+        apply_threshold(img)?
+    };
 
-    img.pixels_mut()
-        .for_each(|x| x.0 = [(255.0 * (x.0[0] as f32 / 255.0).powf(1.0 / gamma_correction)) as u8]);
+    dithered_img.save("/tmp/out_processed.png")?;
 
-    use exoquant::*;
-
-    let palette = vec![Color::new(0, 0, 0, 255), Color::new(255, 255, 255, 255)];
-
-    let ditherer = ditherer::FloydSteinberg::vanilla();
-    let colorspace = SimpleColorSpace::default();
-    let remapper = Remapper::new(&palette, &colorspace, &ditherer);
-
-    let image = img
-        .pixels()
-        .map(|x| Color::new(x.0[0], x.0[0], x.0[0], 255))
-        .collect::<Vec<Color>>();
-
-    let indexed_data = remapper.remap(&image, img.width() as usize);
-
-    debug_print_dithered(&indexed_data, img.width(), img.height())?;
-
-    // convert to vec of line bits
-
-    let mut lines = Vec::new();
-
-    for y in 0..img.height() {
-        let mut line = [0u8; 90];
-
-        for x in 0..img.width() {
-            let i = y * img.width() + x;
-            let i = indexed_data[i as usize];
-
-            let byte = x / 8;
-            let bit = x % 8;
-
-            if i == 0 {
-                line[89 - byte as usize] |= 1 << bit;
-            }
-        }
-
-        lines.push(line);
-    }
-
+    let lines = img_to_lines(dithered_img)?;
     Ok(lines)
 }
 
@@ -242,11 +318,13 @@ fn print_lines(lines: Vec<[u8; 90]>, settings: &Settings) -> Result<(), PrinterB
     ))?;
 
     printer.send_command(PrinterCommand::SetExpandedMode(PrinterExpandedMode {
-        cut_at_end: true,
+        cut_at_end: settings.auto_cut,
         high_resolution_printing: settings.dpi_600,
     }))?;
 
-    printer.send_command(PrinterCommand::SetMode(PrinterMode { auto_cut: true }))?;
+    printer.send_command(PrinterCommand::SetMode(PrinterMode {
+        auto_cut: settings.auto_cut,
+    }))?;
 
     // this is needed for the auto cut
     printer.send_command(PrinterCommand::SetPageNumber(1))?;
@@ -264,18 +342,6 @@ fn print_lines(lines: Vec<[u8; 90]>, settings: &Settings) -> Result<(), PrinterB
     trace!("{:#?}", printer.read_status()?);
     trace!("{:#?}", printer.read_status()?);
     trace!("{:#?}", printer.read_status()?);
-
-    Ok(())
-}
-
-#[allow(dead_code)]
-fn debug_print_dithered(data: &[u8], width: u32, height: u32) -> Result<(), PrinterBotError> {
-    let img = image::ImageBuffer::from_fn(width, height, |x, y| {
-        let i = y * width + x;
-        let i = data[i as usize];
-        image::Rgba([i * 255, i * 255, i * 255, 255])
-    });
-    img.save("/tmp/out_dithered.png")?;
 
     Ok(())
 }
